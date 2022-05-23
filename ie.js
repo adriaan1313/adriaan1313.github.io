496e0edd857f1b8491595886469dbb632041f454
Bunnygamers <adriaan1313@outlook.com>
2022-05-23 20:08:45 +0200

    working some groundlays

---
+++ ie.js
@@ -0,0 +1,2 @@
+var uA = navigator.userAgent.toLowerCase();
+if(uA.indexOf("msie") != -1 | uA.indexOf("trident") != -1) {window.top.location="index_ie.htm";}
---
+++ menu.js
@@ -0,0 +1,157 @@
+const menu = `<div id="menu">
+	<ul>
+		<li style="width: 4em;">
+			<a href="/" style="width: 4em;"><div style="width: 4em;">Home</div></a>
+		</li>
+		<li>
+			<a href="https://github.com/adriaan1313/downloady-bois"><div><span style="font-size: 11pt;line-height:9pt;">Downloady-bois</span></div></a>
+			<ul>
+				<li><a href="https://github.com/adriaan1313/downloady-bois#readme"><div>Please ignore the name</div></a></li>
+				<li>
+					<a href="https://github.com/v3dbot/publitas-lister"><div>Publitas</div></a>
+					<ul>
+						<li><a href="projects/publitas-lister"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/Omroep%20Zeeland"><div>Omroep Zeeland</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/oze"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/audiommicro"><div>AudioMicro</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/audiomicro"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/epidemic"><div>Epidemic Sound</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/epidemic"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/nicknl"><div>Nickelodeon Nederland &amp; België</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/nicknl"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/lalela"><div>Lalela</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/lalela"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/Omroep%20West"><div>Omroep West</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/Omroep%20West"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/Omroep%20Brabant"><div>Omroep Brabant</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/Omroep%20Brabant"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/Omroep%20Fryslan"><div>Omroep Friesland</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/Omroep%20Fryslan"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/downloady-bois/tree/main/RTVNH"><div>RTVNH</div></a>
+					<ul>
+						<li><a href="projects/downloady-bois/RTVNH"><div>Demo</div></a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="#"><div><span style="font-size: 11pt;line-height:9pt;">P5 Stuff</span></div></a>
+			<ul>
+				<li>
+					<a href="https://editor.p5js.org/adriaan1313/sketches/ffMk8HXEu"><div>unblend</div></a>
+					<ul>
+						<li><a href="projects/processing?id=ffMk8HXEu"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				
+				<li>
+					<a href="https://editor.p5js.org/adriaan1313/sketches/Ur4u0HM4p"><div>replace colour</div></a>
+					<ul>
+						<li><a href="projects/processing?id=Ur4u0HM4p"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://editor.p5js.org/adriaan1313/sketches/wuOAPwAY_"><div>squircle</div></a>
+					<ul>
+						<li><a href="projects/processing?id=wuOAPwAY_"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://editor.p5js.org/adriaan1313/sketches/nyq8Ftqo3"><div>sticker bomb</div></a>
+					<ul>
+						<li><a href="projects/processing?id=nyq8Ftqo3"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://editor.p5js.org/adriaan1313/sketches/64I9ZVj11"><div>The Coding Train fire effect modification</div></a>
+					<ul>
+						<li><a href="projects/processing?id=64I9ZVj11"><div>Demo</div></a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="#"><div><span style="font-size: 11pt;line-height:9pt;">Other JS Stuff</span></div></a>
+			<ul>
+				<li><a href="projects/translate.user.js"><div>Translate popup remover<span style="font-size: 10pt;line-height:9pt;"><br>You need a userscript plugin</span></div></a></li>
+				<li><a href="projects/c³"><div>C&sup3;</div></a></li>
+				<li>
+					<a href="https://github.com/adriaan1313/game-idk"><div>game-idk</div></a>
+					<ul>
+						<li><a href="projects/game-idk"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/DTMF-Library"><div>DTMF</div></a>
+					<ul>
+						<li><a href="projects/DTMF"><div>Demo</div></a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://github.com/adriaan1313/SNGCSV"><div>SNGCSV</div></a>
+					<ul>
+						<li><a href="projects/SNGCSV"><div>Demo</div></a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="#"><div>More coding</div></a>
+			<ul>
+				<li><a href="http://imageandarchive.simpsite.nl/"><div>IAA</div></a></li>
+				<li><a href="https://github.com/Gamemode4Dev/GM4_Datapacks"><div>Gamemode 4</div></a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="#"><div>Other Stuff</div></a>
+			<ul>
+				<li><a href="https://github.com/adriaan1313/GM4_Resources_32x32"><div>GM4 Faithful</div></a></li>
+				<li><a href="https://www.youtube.com/channel/UCcGALopqQX4YxuN1StrkPoA"><div>Music</div></a></li>
+				<li>
+					<a><div>School Stuff</div></a>
+					<ul>
+						<li><a href="https://www.youtube.com/watch/FQozB5xdokY"><div>𝕋𝕚𝕜𝕜𝕖𝕣𝕥𝕛𝕖 𝕄𝕖𝕥 𝕄𝕚𝕛𝕟 𝕍𝕣𝕚𝕖𝕟𝕕𝕖𝕟 (𝔾𝔸𝔸𝕋 𝔽𝕆𝕌𝕋!)</div></a></li>
+						<li><a href="https://www.youtube.com/playlist?list=PLKcnTv5nzWmK7T8WpdIRVSoPUmV7s0Dmr"><div>Ls</div></a></li>
+						<li><a href="https://youtu.be/8bmRjcwZbcY"><div>Niet te Filmen slapstick</div></a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>`;
+document.body.innerHTML +=menu;
