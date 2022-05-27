61642297edb9ddc584a7b20bb3ff43fa8fe93649
Bunnygamers <adriaan1313@outlook.com>
2022-05-27 10:14:13 +0200

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -1,4 +1,4 @@
-if(window.location.href.split('?')[1] == "dark") {
+if(window.location.href.split('?')[1].split('#')[0] == "dark") {
   document.getElementById("stylesheet").href = "dark.css";
   document.getElementById("dark").href = ".";
   document.getElementById("dark").innerHTML = "light mode";
