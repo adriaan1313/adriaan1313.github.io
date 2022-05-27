97c1dc181df7016688485fc4ec5bf0459fcd350d
Bunnygamers <adriaan1313@outlook.com>
2022-05-27 10:16:21 +0200

    ffs, why isn't that just an array or something

--- darkMode.js
+++ darkMode.js
@@ -2,5 +2,5 @@ if(window.location.href.split('?')[1].split('#')[0] == "dark") {
   document.getElementById("stylesheet").href = "dark.css";
   document.getElementById("dark").href = ".";
   document.getElementById("dark").innerHTML = "light mode";
-  document.getElementsByClassName("mL").forEach(a=>a.href+="?dark");
+  [...document.getElementsByClassName("mL")].forEach(a=>a.href+="?dark");
 }
